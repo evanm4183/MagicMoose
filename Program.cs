84d23aaca4d8bc8aa777572dbba4d3c8aea16e34
Evan Mitchell <evanm4183@gmail.com>
2022-08-18 15:39:38 -0500

    basic version

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace MagicMoose
 {
@@ -6,7 +8,20 @@ namespace MagicMoose
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<string> expressions = new List<string>()
+            {
+                "As I see it, yes.",
+                "Ask again later.",
+                "Better not tell you now.",
+                "Cannot predict now.",
+                "Concentrate and ask again."
+            };
+
+            Console.Write("Enter a question: ");
+            string question = Console.ReadLine();
+            Random r = new Random();
+
+            Console.WriteLine(expressions[r.Next(0, 5)]);
         }
     }
 }
